cbf8e406a937df4f589d5a9b62b986133ce4fa17
ny <niey@tianji.cn>
2022-06-13 17:50:21 +0800

    视图

---
+++ src/view/layer.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param {*} box
+ * @param {*} node
+ * @returns
+ */
+export function computeLayerViews(box, node) {
+  const { children = [] } = node
+  return new Array(children.length).fill(0).map(() => ({ ...box }))
+}
